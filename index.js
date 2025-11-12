5c81ca3be070ce202f8d2a7e1bab4f846eb7efb5
CRIMSON0731 <alfrancisjuvir@gmail.com>
2025-11-12 15:10:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try {
             // API Call 1: Attempt login. 
-            const response = await fetch('http://localhost:3000/login', {
+            const response = await fetch('https://enrollment-system-production-b592.up.railway.app/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username: currentUsername, password })
